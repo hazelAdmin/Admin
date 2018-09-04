d057e430f11c37523f1d9b183762addd642255f6
sugihp14 <sugihpurnama14@gmail.com>
2018-09-04 11:53:40 +0700

    update

--- admin.php
+++ admin.php
@@ -84,6 +84,7 @@ if( empty( $_SESSION['id_user'] ) ){
 	?>
       <!-- Main component for a primary marketing message or call to action -->
 	  
+	  
       <div class="jumbotron">
         <h2>Selamat Datang di Hazel Photo House</h2>
 
